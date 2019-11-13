c1c4a0c96502cae013612b76862cc2fb8cd269e0
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2019-11-13 20:59:19 +0100

    Update non-admin-install.ps1

--- non-admin-install.ps1
+++ non-admin-install.ps1
@@ -1,4 +1,9 @@
-$FileName = "system-info.ps1"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/master/$FileName -OutFile $FileName 
-. ./$FileName
-Remove-Item $FileName
+function sourceRemote ($filename){
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/master/$FileName -OutFile $FileName   
+Write-Host "XXX" $FileName
+. ./$FileName 
+Remove-Item $FileName  
+}
+
+sourceRemote "system-info.ps1"
