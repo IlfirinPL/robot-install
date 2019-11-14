4453c0358f08a0b93a9322f8f3f06f39b98f2c67
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2019-11-14 10:30:00 +0100

    Delete admin-install.ps1

--- admin-install.ps1
+++
@@ -1,9 +0,0 @@
-function sourceRemote ($filename){
-
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/master/$FileName -OutFile $FileName   
-Write-Host "XXX" $FileName
-. ./$FileName 
-Remove-Item $FileName  
-}
-
-sourceRemote "system-info.ps1"
