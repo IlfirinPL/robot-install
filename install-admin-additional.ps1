4927719e26ba83bc5ce40fd6df355f1df3c1f9d5
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2019-11-20 10:06:22 +0100

    Create install-admin-additional.ps1

---
+++ install-admin-additional.ps1
@@ -0,0 +1,6 @@
+choco install vscode -y
+choco install microsoft-visual-cpp-build-tools -y
+choco install dbeaver -y
+choco install tortoisegit -y
+choco install doublecmd -y
+choco install imagemagick.tool -y
