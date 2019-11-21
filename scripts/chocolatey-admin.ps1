944eadde89d0128b5cc46012f0333a4b0eaf1f25
Marcin Koperski <marcin.koperski+github@gmail.com>
2019-11-21 10:39:07 +0100

    Create chocolatey-admin.ps1

---
+++ scripts/chocolatey-admin.ps1
@@ -0,0 +1,5 @@
+Write-Output "####### Setup Chocolatey #######"
+if (-Not (Get-Command "choco" -ErrorAction SilentlyContinue) ) { 
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+choco --version 
