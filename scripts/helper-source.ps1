e254ffa5370a8e1d06cf21ca0e8f1be95d6343f7
Marcin Koperski <marcin.koperski+github@gmail.com>
2019-11-21 10:25:15 +0100

    Update helper-source.ps1

--- scripts/helper-source.ps1
+++ scripts/helper-source.ps1
@@ -1,4 +1,8 @@
-function sourceRemote ($url) { Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri $url -OutFile $env:tmp/tmp.ps1 ; & "$env:tmp/tmp.ps1" ; }
+<#  
+to import to shell this helper paste this
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/master/scripts/helper-source.ps1 -OutFile $env:temp/import.ps1;Set-ExecutionPolicy Bypass -Scope Process -Force; Import-module $env:temp/import.ps1 -Force
+#>
+function sourceRemote ($url) { Invoke-WebRequest -Uri $url -OutFile $env:tmp/tmp.ps1 ; Set-ExecutionPolicy Bypass -Scope Process -Force; & "$env:tmp/tmp.ps1" ; }
 function copyRemote ($url, $destPath) { Invoke-WebRequest -Uri $url -OutFile $destPath }
 function timerStart () { $env:StartDate = (GET-DATE) }
 function timerStop ($msg) { Write-Output "$msg $(NEW-TIMESPAN -Start $env:StartDate -End (GET-DATE))" }
