30e5134e47aee2427091ca517ace0b45d05c1068
Marcin Koperski <marcin.koperski+github@gmail.com>
2019-11-21 09:27:57 +0100

    Update helper-source.ps1

--- scripts/helper-source.ps1
+++ scripts/helper-source.ps1
@@ -1,3 +1,16 @@
-function sourceRemote ($url) { Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri $url -OutFile $env:tmp/tmp.ps1 ;   & "$env:tmp/tmp.ps1" ; }
-function copyRemote ($url,$destPath) { Invoke-WebRequest -Uri $url -OutFile $destPath  }
+function sourceRemote ($url) { 
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    Invoke-WebRequest -Uri $url -OutFile $env:tmp/tmp.ps1 ; 
+    & "$env:tmp/tmp.ps1" ; 
+}
+function copyRemote ($url, $destPath) { 
+    Invoke-WebRequest -Uri $url -OutFile $destPath 
+}
 
+function timerStart () {
+    $env:StartDate = (GET-DATE)
+}
+
+function timerStop ($msg) {
+    Write-Output "$msg $(NEW-TIMESPAN -Start $env:StartDate -End (GET-DATE))"
+}
