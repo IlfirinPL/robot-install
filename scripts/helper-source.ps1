6b728b695687e7d513414df284195b6b20637ee0
Marcin Koperski <marcin.koperski+github@gmail.com>
2019-11-21 10:48:54 +0100

    Update helper-source.ps1

--- scripts/helper-source.ps1
+++ scripts/helper-source.ps1
@@ -3,6 +3,7 @@ to import to shell this helper paste this
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/master/scripts/helper-source.ps1 -OutFile $env:temp/import.ps1;Set-ExecutionPolicy Bypass -Scope Process -Force; Import-module $env:temp/import.ps1 -Force
 #>
 function sourceRemote ($url) { Invoke-WebRequest -Uri $url -OutFile $env:tmp/tmp.ps1 ; Set-ExecutionPolicy Bypass -Scope Process -Force; & "$env:tmp/tmp.ps1" ; }
+function sourceRemoteExp ($url) { iex ((New-Object System.Net.WebClient).DownloadString($url)) }
 function copyRemote ($url, $destPath) { Invoke-WebRequest -Uri $url -OutFile $destPath }
 function timerStart () { $env:StartDate = (GET-DATE) }
 function timerStop ($msg) { Write-Output "$msg $(NEW-TIMESPAN -Start $env:StartDate -End (GET-DATE))" }
