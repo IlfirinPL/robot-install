f0909c7ab35550c34f0f6c9ce62facfe6a6c865e
Marcin Koperski <marcin.koperski+github@gmail.com>
2019-11-21 13:22:35 +0100

    Update git-install-setup.ps1

--- scripts/git-install-setup.ps1
+++ scripts/git-install-setup.ps1
@@ -1,11 +1,8 @@
 
-$env:proxyHostTemp="spplapp10200:8080"
+$proxyHostTemp = "spplapp10200:8080"
 
-$SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "proxy" 2> $null
-if ($SEL -ne $null) {
-    Write-Output "####### Setup Git PROXY ####### ***SKIP***"
-}
-else {
+
+if (-not ( Test-Path $env:HOMEDRIVE/.gitconfig)) {
     Write-Output "####### Setup Git PROXY #######"
     $defaultValue = $env:USERNAME
     Write-Output "####### Get Credentials for Git PROXY #######"
@@ -14,27 +11,48 @@ else {
     $password = Read-Host "Enter password for CK $login" -AsSecureString
     $password = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
     $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto($password)
-
-}
-
-
-$SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "email"  2> $null
-if ($SEL -ne $null) {
-    Write-Output "####### Setup Git EMAIL ####### ***SKIP***"
+    $email = Read-Host "Provided Company Email"
+    New-Item -Path $env:APPDATA -Name "email.ini" -ItemType "file" -Force -Value "$email" > $null
 }
 else {
-    Write-Output "####### Setup Git EMAIL #######"
-    if (Test-Path $env:APPDATA/email.ini) {
-        $defaultValue = Get-Content -Path $env:APPDATA/email.ini
-        $prompt = Read-Host "Press enter to accept default email adress [$($defaultValue)]"
-        $email = ($defaultValue, $prompt)[[bool]$prompt]
-        New-Item -Path $env:APPDATA -Name "email.ini" -ItemType "file" -Force -Value "$email" > $null
+
+    $SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "proxy" 2> $null
+    if ($SEL -ne $null) {
+        Write-Output "####### Setup Git PROXY ####### ***SKIP***"
     }
     else {
-        $email = Read-Host "Provided Company Email"
-        New-Item -Path $env:APPDATA -Name "email.ini" -ItemType "file" -Force -Value "$email" > $null
+        Write-Output "####### Setup Git PROXY #######"
+        $defaultValue = $env:USERNAME
+        Write-Output "####### Get Credentials for Git PROXY #######"
+        $prompt = Read-Host "Press enter to accept to default login [$($defaultValue)]"
+        $login = ($defaultValue, $prompt)[[bool]$prompt]
+        $password = Read-Host "Enter password for CK $login" -AsSecureString
+        $password = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+        $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto($password)
+
     }
+
+
+    $SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "email"  2> $null
+    if ($SEL -ne $null) {
+        Write-Output "####### Setup Git EMAIL ####### ***SKIP***"
+    }
+    else {
+        Write-Output "####### Setup Git EMAIL #######"
+        if (Test-Path $env:APPDATA/email.ini) {
+            $defaultValue = Get-Content -Path $env:APPDATA/email.ini
+            $prompt = Read-Host "Press enter to accept default email adress [$($defaultValue)]"
+            $email = ($defaultValue, $prompt)[[bool]$prompt]
+            New-Item -Path $env:APPDATA -Name "email.ini" -ItemType "file" -Force -Value "$email" > $null
+        }
+        else {
+            $email = Read-Host "Provided Company Email"
+            New-Item -Path $env:APPDATA -Name "email.ini" -ItemType "file" -Force -Value "$email" > $null
+        }
     
+    }
+
+
 }
 
 
@@ -46,21 +64,28 @@ if (-Not (Get-Command "git" -ErrorAction SilentlyContinue)) {
 } 
 git --version
 
-
-$SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "proxy" 2> $null
-if ($SEL -ne $null) {
-}
-else {
-
-    git config --global http.proxy ${login}:${password}@$(env:proxyHostTemp)
+if (-not ( Test-Path $env:HOMEDRIVE/.gitconfig)) {
+    git config --global http.proxy ${login}:${password}@$proxyHostTemp
     git config --global http.sslVerify false 
-}
-
-
-$SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "email" 2> $null
-if ($SEL -ne $null) {
-}
-else {
- 
     git config --global user.email "${email}" 
 }
+else {
+    $SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "proxy"
+    if ($SEL -ne $null) {
+    }
+    else {
+
+        git config --global http.proxy ${login}:${password}@$proxyHostTemp
+        git config --global http.sslVerify false 
+    }
+
+
+    $SEL = Select-String -Path $env:HOMEDRIVE/.gitconfig -Pattern "email"
+    if ($SEL -ne $null) {
+    }
+    else {
+ 
+        git config --global user.email "${email}" 
+    }
+}
+
