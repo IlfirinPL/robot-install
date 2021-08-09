8b7dbe4a52841aa5ad15bd12c0de948cbb693740
Koperski,  (Marcin) <marcin.koperski1@nn.pl>
2021-08-09 11:56:45 +0200

    remove some legacy solutions

--- install-admin.ps1
+++ install-admin.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/IlfirinPL/robot-install/raw/master/scripts/helper-source.ps1 -OutFile $env:temp/import.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; Import-module $env:temp/import.ps1 -Force
 
 if (!(Test-Path Env:PY_PYTHON)) {
-    $Env:PY_PYTHON = "3.8"
+    $Env:PY_PYTHON = "3.9"
 }
 
 if (!(Test-Path Env:branch)) {
--- scripts/python-downlad-install.ps1
+++ scripts/python-downlad-install.ps1
@@ -1,5 +1,5 @@
-$url = "https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe"
+$url = "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe"
 $output = "$env:USERPROFILE/Downloads"
 $start_time = Get-Date; Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output; Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-& "$env:USERPROFILE/Downloads/python-3.9.4-amd64.exe"
+& "$env:USERPROFILE/Downloads/python-3.9.6-amd64.exe"
