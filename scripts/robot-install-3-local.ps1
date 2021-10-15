ce3f812c049fe8c70c44eab743f56593b46508a6
Marcin Koperski <marcin.koperski+github@gmail.com>
2021-10-15 18:21:20 +0200

    Update robot-install-3-local.ps1

--- scripts/robot-install-3-local.ps1
+++ scripts/robot-install-3-local.ps1
@@ -1,5 +1,6 @@
 ./helper-source.ps1
 py -m ensurepip
+py -m pip install -U pip wheel setuptools
 py -m pip install -U -r ./../data/packages.txt
 py -m pip install -U -r ./../data/packages-robot.txt
 py -m pip install -U -r ./../data/ide.txt
