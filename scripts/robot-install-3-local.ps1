b969383179b8d956d4a1090d484a11932225e7b3
Ilfirin <marcin.koperski+git@gmail.com>
2021-10-29 08:47:44 +0200

    move file to root

--- scripts/robot-install-3-local.ps1
+++
@@ -1,6 +0,0 @@
-./helper-source.ps1
-py -m ensurepip
-py -m pip install -U pip wheel setuptools
-py -m pip install -U -r ./../data/packages.txt
-py -m pip install -U -r ./../data/packages-robot.txt
-py -m pip install -U -r ./../data/ide.txt
