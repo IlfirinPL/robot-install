8cb932a36899663c864dfe07b4142f15cecf6612
Ilfirin <marcin.koperski+git@gmail.com>
2021-11-13 23:23:20 +0100

    update script to install python

--- scripts/python-downlad-install.ps1
+++ scripts/python-downlad-install.ps1
@@ -1,5 +1,8 @@
-$url = "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe"
-$output = "$env:USERPROFILE/Downloads"
+$version = "3.9.8"
+$url = "https://www.python.org/ftp/python/" + $version + "/python-" + $version + "-amd64.exe"
+$output = "$env:TEMP"
 $start_time = Get-Date; Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output; Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-& "$env:USERPROFILE/Downloads/python-3.9.6-amd64.exe"
+Start-BitsTransfer -Source ${url} -Destination ${output}; Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+$cmd = "$output/python-" + ${version} + "-amd64.exe"
+$cmd | Invoke-Expression
+Write-Output "Remove-Item  $cmd"
