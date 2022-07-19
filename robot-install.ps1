d8fcae95012fc5e5126c8ac9b9a1227ccbaf1a43
Ilfirin <marcin.koperski+git@gmail.com>
2022-07-19 20:02:15 +0200

    cleanup not required dep

--- robot-install.ps1
+++ robot-install.ps1
@@ -3,6 +3,5 @@ py -m pip install -U pip wheel setuptools
 py -m pip install -U --upgrade-strategy eager `
     -r ./data/packages-robot.txt `
     -r ./data/ide.txt `
-    -r ./data/rpa.txt `
 
 py -m webdrivermanager chrome firefox
