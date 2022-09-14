f82b01b159f75dd4b9d23373b6239b40e6d67134
Ilfirin <marcin.koperski+git@gmail.com>
2022-09-14 19:22:32 +0200

    add tools

--- robot-install.ps1
+++ robot-install.ps1
@@ -3,5 +3,6 @@ py -m pip install -U pip setuptools
 py -m pip install -U --upgrade-strategy eager `
     -r ./data/packages-robot.txt `
     -r ./data/ide.txt `
+    -r ./data/support_tools.txt
 
 py -m webdrivermanager chrome firefox
--- robot-install.sh
+++ robot-install.sh
@@ -3,6 +3,6 @@ python3 -m pip install -U \
 --upgrade-strategy eager \
 -r ./data/packages-robot.txt \
 -r ./data/ide.txt \
--r ./data/rpa.txt \
+-r ./data/support_tools.txt
 
 python3 -m webdrivermanager chrome firefox
