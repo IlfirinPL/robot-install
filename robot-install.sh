02f629fe37d7b654be391cc8ac4d1ad620176d36
Ilfirin <marcin.koperski+github@gmail.com>
2022-10-16 19:26:26 +0200

    switch to python 3.10

--- robot-install.sh
+++ robot-install.sh
@@ -1,8 +1,8 @@
-python3 -m pip install -U pip
-python3 -m pip install -U \
+python3.10 -m pip install -U pip
+python3.10 -m pip install -U \
 --upgrade-strategy eager \
 -r ./data/packages-robot.txt \
 -r ./data/ide.txt \
 -r ./data/support_tools.txt
 
-python3 -m webdrivermanager chrome firefox
+python3.10 -m webdrivermanager chrome firefox
