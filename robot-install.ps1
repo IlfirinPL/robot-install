7fbe3b53ddd4366fd2201f1c3860e6d96f8e19ab
Ilfirin <marcin.koperski+git@gmail.com>
2022-11-04 18:37:03 +0100

    update to python 3.11

--- robot-install.ps1
+++ robot-install.ps1
@@ -7,6 +7,9 @@ py -m pip install -U pip setuptools wheel
 py -m pip install --upgrade `
     -r ./data/packages-robot.txt `
     -r ./data/ide.txt `
-    -r ./data/support_tools.txt
+    -r ./data/support_tools.txt `
+    ./bin/lxml-4.9.0-cp311-cp311-win_amd64.whl
 
 py -m webdrivermanager chrome firefox
+
+py -m robot.run --version
