b5fd861baf231335759548ed9a75fab1a964f6b7
Marcin Koperski <marcin.koperski+github@gmail.com>
2022-11-15 10:04:21 +0000

    add dev container

--- robot-install.sh
+++ robot-install.sh
@@ -1,8 +1,12 @@
-python3.10 -m pip install -U pip
-python3.10 -m pip install -U \
+alias python="python3.10"
+
+python --version
+
+python -m pip install -U pip
+python -m pip install -U \
 --upgrade-strategy eager \
 -r ./data/packages-robot.txt \
 -r ./data/ide.txt \
 -r ./data/support_tools.txt
 
-python3.10 -m webdrivermanager chrome firefox
+python -m webdrivermanager chrome firefox
