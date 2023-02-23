6b5a86444976580c32c5c325d1d1f115ada8e2b5
Ilfirin <marcin.koperski+git@gmail.com>
2023-02-23 12:50:32 +0100

    update version

--- robot-install.ps1
+++ robot-install.ps1
@@ -2,7 +2,6 @@ py -m ensurepip
 py -m pip install -U pip setuptools wheel
 
 # binary install https://www.lfd.uci.edu/~gohlke/pythonlibs
-# https://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
 
 py -m pip install --upgrade `
     -r ./data/packages-robot.txt `
